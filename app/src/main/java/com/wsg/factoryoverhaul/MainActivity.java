b6b80789401e1c142bc1ebe62976ad048e87b890
wushange <309646776@qq.com>
2019-05-18 22:09:01 +0800

    add data

--- app/src/main/java/com/wsg/factoryoverhaul/MainActivity.java
+++ app/src/main/java/com/wsg/factoryoverhaul/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wsg.factoryoverhaul;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,11 +13,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.aliyun.svideo.sdk.external.struct.common.CropKey;
 import com.aliyun.svideo.sdk.external.struct.recorder.CameraType;
 import com.aliyun.svideo.sdk.external.struct.snap.AliyunSnapVideoParam;
 import com.blankj.utilcode.util.ActivityUtils;
+import com.blankj.utilcode.util.FileIOUtils;
 import com.blankj.utilcode.util.FileUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.wsg.factoryoverhaul.bean.User;
 
@@ -33,11 +40,13 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.rclview)
     RecyclerView rclview;
 
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mContext = this;
 
         videoAdapter = new VideoAdapter(new ArrayList<File>());
         rclview.setLayoutManager(new LinearLayoutManager(getBaseContext()));
@@ -48,6 +57,76 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getBaseContext(), VideoActivity.class);
                 intent.putExtra("PATH", videoAdapter.getItem(position).getPath());
                 ActivityUtils.startActivity(intent);
+            }
+        });
+        videoAdapter.setOnItemLongClickListener(new BaseQuickAdapter.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(BaseQuickAdapter adapter, View view, int position) {
+                File file = videoAdapter.getItem(position);
+                new MaterialDialog.Builder(mContext)
+                        .title("选择操作")
+                        .items("修改名称", "删除", "上传")
+                        .itemsCallback(new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog dialog, View itemView, int position, CharSequence text) {
+                                switch (position) {
+                                    case 0:
+                                        new MaterialDialog.Builder(mContext)
+                                                .title("修改名称")
+                                                .input("输入修改名称", file.getName(), new MaterialDialog.InputCallback() {
+                                                    @Override
+                                                    public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
+
+                                                    }
+                                                }).positiveText("确定")
+                                                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                                    @Override
+                                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                                                        dialog.dismiss();
+                                                        FileUtils.rename(file, dialog.getInputEditText().getText().toString());
+                                                        ToastUtils.showShort("修改成功！");
+                                                        getData();
+                                                    }
+                                                })
+                                                .negativeText("取消").show();
+                                        break;
+                                    case 1:
+                                        new MaterialDialog.Builder(mContext)
+                                                .title("确认删除这条视频吗？")
+                                                .positiveText("确定")
+                                                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                                    @Override
+                                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                                                        dialog.dismiss();
+                                                        FileUtils.delete(file);
+                                                        ToastUtils.showShort("删除成功！");
+                                                        getData();
+                                                    }
+                                                })
+                                                .negativeText("取消").show();
+                                        break;
+                                    case 2:
+                                        MaterialDialog m = new MaterialDialog.Builder(mContext)
+                                                .content("上传中请稍后...")
+                                                .progress(true, 0)
+                                                .show();
+                                        new Handler().postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                m.dismiss();
+                                                ToastUtils.showShort("上传成功！");
+                                                getData();
+                                            }
+
+                                        }, 3000);
+                                        break;
+                                    default:
+                                }
+                            }
+                        }).show();
+                return true;
             }
         });
         getData();

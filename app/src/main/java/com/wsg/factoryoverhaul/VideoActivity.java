8735818733cf94b35530d8173ef3250c7512d97d
wushange <309646776@qq.com>
2019-05-25 22:14:51 +0800

    add ijk

--- app/src/main/java/com/wsg/factoryoverhaul/VideoActivity.java
+++ app/src/main/java/com/wsg/factoryoverhaul/VideoActivity.java
@@ -1,36 +1,43 @@
 package com.wsg.factoryoverhaul;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.blankj.utilcode.util.FileUtils;
-import com.wsg.factoryoverhaul.util.Contants;
-import com.wsg.factoryoverhaul.view.JzvdStdSpeed;
+import com.wsg.factoryoverhaul.bean.SpeedEvent;
+import com.wsg.factoryoverhaul.view.MyIJKMediaSystem;
+import com.wsg.factoryoverhaul.view.MyJZMediaSystem;
+import com.wsg.factoryoverhaul.view.MyJZVideoPlayerStandard;
 
-import java.io.File;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
-import cn.jzvd.JZDataSource;
-import cn.jzvd.Jzvd;
-import cn.jzvd.JzvdStd;
+import cn.jzvd.JZVideoPlayer;
 
 public class VideoActivity extends AppCompatActivity  {
     String filePth = "";
-
+    private MyJZVideoPlayerStandard mPlayerStandard;
+    //系统播放器引擎
+    MyJZMediaSystem mJZMediaSystem;
+    MyIJKMediaSystem mIJKMediaSystem;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        mJZMediaSystem = new MyJZMediaSystem();
+        mIJKMediaSystem = new MyIJKMediaSystem();
+        mPlayerStandard = findViewById(R.id.videoplayer);
         Intent intent = getIntent();
         if (intent != null) {
             Bundle bundle = intent.getExtras();
             if (bundle != null) {
                 filePth = bundle.getString("PATH");
-                JzvdStdSpeed jzvdStd = findViewById(R.id.videoplayer);
-                JZDataSource jzDataSource = new JZDataSource(filePth, FileUtils.getFileName(filePth));
-                jzvdStd.setUp(jzDataSource,JzvdStd.SCREEN_FULLSCREEN);
-//                JzvdStd.startFullscreenDirectly(this, JzvdStd.class,jzDataSource);
+                mPlayerStandard.setUp(filePth,JZVideoPlayer.SCREEN_WINDOW_FULLSCREEN, FileUtils.getFileName(filePth));
             }
         }
 
@@ -39,15 +46,56 @@ public class VideoActivity extends AppCompatActivity  {
 
     @Override
     public void onBackPressed() {
-        if (Jzvd.backPress()) {
+        if (JZVideoPlayer.backPress()) {
             return;
         }
         super.onBackPressed();
     }
-
     @Override
     protected void onPause() {
         super.onPause();
-        Jzvd.resetAllVideos();
+        JZVideoPlayer.releaseAllVideos();
+        JZVideoPlayer.setMediaInterface(mIJKMediaSystem);
+        //Change these two variables back
+        JZVideoPlayer.FULLSCREEN_ORIENTATION = ActivityInfo.SCREEN_ORIENTATION_SENSOR;
+        JZVideoPlayer.NORMAL_ORIENTATION = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        JZVideoPlayer.setMediaInterface(mIJKMediaSystem);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //注册消息总线
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**倍速切换*/
+    @Subscribe(threadMode = ThreadMode.POSTING)
+    public void onMessageEventPostSpeed(SpeedEvent event) {
+        mJZMediaSystem.setSpeeding(event.getSpeed());
+        mIJKMediaSystem.setSpeeding(event.getSpeed());
+        Toast.makeText(this, "正在切换倍速:"+event.getSpeed(), Toast.LENGTH_LONG).show();
+    }
+
+
+
+
 }
